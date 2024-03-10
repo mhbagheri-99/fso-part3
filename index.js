6476edbe8465de9c3195cba7f3dcdaabc4952207
mhbagheri-99 <mhbagheri3@gmail.com>
2024-03-10 11:23:35 +0100

    uibuild

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get('/api/persons/:id', (request, response, next) => {
   }).catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name || !body.number) {
@@ -53,7 +53,7 @@ app.post('/api/persons', (request, response) => {
     
       person.save().then(savedPerson => {
         response.json(savedPerson)
-      })
+      }).catch(error => next(error))
     }
   })
 })
@@ -66,11 +66,11 @@ app.put('/api/persons/:id', (request, response, next) => {
     number: body.number,
   }
 
-  Contact.findByIdAndUpdate(request.params.id, person, { new: true })
+  Contact.findByIdAndUpdate(request.params.id, person, 
+    { new: true, runValidators: true, context: 'query' })
     .then(updatedPerson => {
       response.json(updatedPerson)
-    })
-    .catch(error => next(error))
+    }).catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -98,7 +98,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
